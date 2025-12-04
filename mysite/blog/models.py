1531d88c432830a8dcf5fff8a7c65d63dbb22b7c
YuriGConstantino <yuri.gconstantino@gmail.com>
2025-12-04 20:36:24 -0300

    create admin models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+STATUS = (
+    (0, 'Draft'),
+    (1, 'Published')
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
